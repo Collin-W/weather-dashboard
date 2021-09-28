be01dcd5ddb118ce27ac5157e59328b90c99210c
Collin-W <whalencollin@gmail.com>
2021-09-27 19:39:06 -0500

    working on fixing the html layout

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,26 @@
-function weatherData() {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
-    .this(response => response.json())
-    .this(data)
-}
+// var lat = 44.98;
+// var lon = -93.25;
+
+// function weatherData() {
+// //     fetch('https://api.openweathermap.org/data/2.5/onecall?'
+
+
+    
+// //    + 'lat=' + JSON.stringify(lat)
+// //    + '&lon=' + JSON.stringify(lon)
+
+  
+// //    + '&appid=a278107ddd3948f0d3f900ddd0dc1432')
+
+
+
+
+// fetch('api.openweathermap.org/data/2.5/weather?zip=55109,us&appid='
+
+// + '&appid=a278107ddd3948f0d3f900ddd0dc1432')
+
+//     .this(response => response.json())
+//     .this(response => console.log(response.data))
+// };
+
+// weatherData();
