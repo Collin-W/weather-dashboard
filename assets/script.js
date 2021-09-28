d257b143e402da26a2ff7876b1bac70fee505c70
Collin-W <whalencollin@gmail.com>
2021-09-28 16:20:23 -0500

    API is returning a response

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,38 @@
 // };
 
 // weatherData();
+
+
+
+// Minneapolis, Minnesota
+
+
+
+function weatherSearch() {
+
+var searchCity = document.querySelector('#search-city').value.trim();
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='
+
++ searchCity
+//+ 'Minneapolis, Minnesota'
+
++ '&appid=a278107ddd3948f0d3f900ddd0dc1432')
+.then(response => response.json())
+.then(response => console.log(response))
+
+};
+
+
+weatherSearch();
+
+function searchButtonHandler() {
+
+    
+
+$('button#search').onclick(weatherSearch())
+
+}
+
+//button click
+// value of button passed into weatherSearch para
