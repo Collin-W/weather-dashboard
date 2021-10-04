f4c1cbc2a275ca35fcc3d6ca4d1f7a041cfdf439
Collin-W <whalencollin@gmail.com>
2021-10-04 10:37:44 -0500

    code clean up

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,18 @@
 var dash = document.querySelector("#city-dash");
-var counter = 0;
 var dataArray = [];
 
 
-
-//accepts the location argument into the fetch function, returning a temperature of location converted to fahrenheit
+//accepts the location argument into the fetch function
 function weatherSearch(location) {
 
-    //var fetchURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + + '&appid=a278107ddd3948f0d3f900ddd0dc1432';
-    //https://api.openweathermap.org/data/2.5/weather?q=
     fetch('https://api.openweathermap.org/data/2.5/weather?q='
 
+           + location
 
-            //+ 'Minneapolis, Minnesota'
-            +
-            location
-
-            +
-            '&appid=a278107ddd3948f0d3f900ddd0dc1432')
+           + '&appid=a278107ddd3948f0d3f900ddd0dc1432')
 
         .then(response => response.json())
         .then(response => {
-
 
             var cityName = location
 
@@ -30,28 +21,15 @@ function weatherSearch(location) {
 
             var kelvinData = response.main.temp;
             var humidityData = response.main.humidity;
-            console.log(humidityData)
             var pressureData = response.main.pressure;
-            console.log(pressureData)
             var windSpeedData = response.wind.speed;
-            console.log(JSON.stringify(windSpeedData))
-
             var iconData = response.weather[0].main.toLowerCase();
-            // change this 
-            //var iconData = response.list[0].weather.main.toLowerCase();;
-            console.log(iconData)
-            //
 
-
+            // convert kelvin temp int to fahrenheit temp int
             var kelvinToFahrenheit = ((kelvinData - 273.15) * 9 / 5 + 32);
-            // var test = System.out.printIn(kelvinToFahrenheit)
-            // console.log(test)
             var fahrenheitTemp = Math.round(kelvinToFahrenheit);
-            console.log(fahrenheitTemp);
-            renderWeatherSearch(cityName, dateTime,
-                fahrenheitTemp, humidityData, pressureData, windSpeedData, iconData);
-            //currentData,
 
+            renderWeatherSearch(cityName, dateTime, fahrenheitTemp, humidityData, pressureData, windSpeedData, iconData);
         });
 };
 
@@ -59,122 +37,92 @@ function weatherSearch(location) {
 var renderWeatherSearch = function (city, date,
     temp, humid, pres, wind, icon) {
 
-        
-
-
     //render icons
     if (icon.startsWith("cloud") === true) {
-        icon = "fas fa-cloud"
+        icon = "fas fa-cloud";
 
     } else if (icon.startsWith("sun") === true) {
-        icon = "far fa-snowflake"
+        icon = "far fa-snowflake";
 
     } else if (icon.startsWith("clear") === true) {
-        icon = "fas fa-sun"
+        icon = "fas fa-sun";
 
     } else if (icon.startsWith("rain") === true) {
-        icon = "fas fa-cloud-showers-heavy"
+        icon = "fas fa-cloud-showers-heavy";
 
     } else if (icon.startsWith("mist") === true) {
-        icon = "fas fa-smog"
+        icon = "fas fa-smog";
 
     } else if (icon.startsWith("snow") === true) {
-        icon = "far fa-snowflake"
+        icon = "far fa-snowflake";
 
     } else if (icon.startsWith("partly") === true) {
-        icon = "fas fa-cloud-sun"
+        icon = "fas fa-cloud-sun";
 
     } else {
         console.log("else");
     };
 
     var cityName = document.createElement('p');
-    cityName.setAttribute("id", "")
-    cityName.setAttribute("class", "reset")
+    cityName.setAttribute("id", "");
+    cityName.setAttribute("class", "");
     cityName.textContent = "" + city + "";
     dash.appendChild(cityName);
 
     var dateTime = document.createElement('p');
     dateTime.setAttribute("id", "");
-    dateTime.setAttribute("class", "reset");
+    dateTime.setAttribute("class", "");
     dateTime.textContent = date;
     dash.appendChild(dateTime);
 
     var cityTemp = document.createElement('p');
     cityTemp.setAttribute("id", "city-temp");
-    cityTemp.setAttribute("class", "reset");
+    cityTemp.setAttribute("class", "");
     cityTemp.textContent = "Temp: " + temp + "°F";
     dash.appendChild(cityTemp);
 
     var cityHumidity = document.createElement('p');
-    cityHumidity.setAttribute("id", "")
-    cityHumidity.setAttribute("class", "reset")
+    cityHumidity.setAttribute("id", "");
+    cityHumidity.setAttribute("class", "");
     cityHumidity.textContent = "Humidity: " + humid + "%";
     dash.appendChild(cityHumidity);
 
     var cityPressure = document.createElement('p');
-    cityPressure.setAttribute("id", "")
-    cityPressure.setAttribute("class", "reset")
+    cityPressure.setAttribute("id", "");
+    cityPressure.setAttribute("class", "");
     cityPressure.textContent = "Pressure: " + pres + "mb";
     dash.appendChild(cityPressure);
 
     var cityWind = document.createElement('p');
-    cityWind.setAttribute("id", "")
-    cityWind.setAttribute("class", "reset")
+    cityWind.setAttribute("id", "");
+    cityWind.setAttribute("class", "");
     cityWind.textContent = "Wind: " + wind + "mph";
     dash.appendChild(cityWind);
 
-
-    
-    var iconLogo = document.createElement('i')
-    var h1Tag = document.createElement('h1')
-    h1Tag.setAttribute("class" , "reset")
-    iconLogo.setAttribute("class", icon)
-    iconLogo.setAttribute("id", "reset")
-    //iconLogo.setAttribute("aria-hidden", "true")
+    var iconLogo = document.createElement('i');
+    var h1Tag = document.createElement('h1');
+    h1Tag.setAttribute("class", "");
+    iconLogo.setAttribute("class", icon);
+    iconLogo.setAttribute("id", "");
     dash.appendChild(h1Tag);
     h1Tag.appendChild(iconLogo);
     console.log(dash.appendChild(iconLogo));
-   
-
-    //    var iconLogo =  iconLogo.innerHTML(icon)
-    //    //document.createElement('i');
-    //    //iconLogo.setAttribute("id", "")
-
-    //    iconLogo.setAttribute("aria-hidden", "true")
-    //    //icon.textContent= icon;
-    //    dash.appendChild(iconLogo);
 };
 
 
-// var = document.createElement('p');
-//    .setAttribute("id", "")
-//    .setAttribute("class", "")
-//    .textContent= "" + + "";
-//    dash.appendChild();
-
 function forecastWeatherCards(location) {
-
 
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='
 
-            +
-            location
+           + location
 
-            +
-            '&appid=a278107ddd3948f0d3f900ddd0dc1432')
+           + '&appid=a278107ddd3948f0d3f900ddd0dc1432')
 
         .then(response => response.json())
         .then(response => {
 
-
             var list = response.list;
             console.log(list)
-
-
-            // var newList = list.filter(date => list.dt_txt = list.dt_txt.includes('12:00:00'))
-            // console.log(newList)
-
 
             for (var i = 0; i < list.length; i++) {
 
@@ -189,16 +137,12 @@ function forecastWeatherCards(location) {
                     var wind = list[i].wind.speed
                     var icon = list[i].weather.icon
 
-
-
                     var dataObject = {
                         'date': date,
                         'temp': temp
                     };
 
                     dataArray.push(dataObject);
-                    console.log(dataArray);
-
                 };
             };
             renderForecastCards(dataArray)
@@ -206,49 +150,43 @@ function forecastWeatherCards(location) {
 };
 
 
-
 function renderForecastCards(dataArray) {
 
     for (var i = 0; i < dataArray.length; i++) {
 
         var cardDate = document.createElement('p');
-        //    cardDate.setAttribute("id", "")
-        //    cardDate.setAttribute("class", "")
+        cardDate.setAttribute("id", "");
+        cardDate.setAttribute("class", "");
         cardDate.textContent = dataArray[i].date;
-        $('#' + i).append(cardDate)
-        //i.toString()
-        //     var cardTemp = document.createElement('p');
-        // //    .setAttribute("id", "")
-        // //    .setAttribute("class", "")
-        // cardTemp.textContent= temp;
+        $('#' + i).append(cardDate);
+
+        var cardTemp = document.createElement('p');
+        cardTemp.setAttribute("id", "")
+        cardTemp.setAttribute("class", "")
+        cardTemp.textContent= temp;
     }
 }
-
 
 //Listens for the search button click, then sends the value of the search textarea to the weatherSearch function
 $('#search').click(function () {
     var searchCity = document.querySelector('#search-city').value.trim();
+    $('#search-city').empty();
     weatherSearch(searchCity);
     forecastWeatherCards(searchCity);
 });
 
 $('button').click(function () {
     var btnValue = $(this).attr('value');
-    $('.reset').empty();
-    // var resetIcon = document.getElementById('reset');
-    // resetIcon.remove();
+    $("#city-dash").empty();
     weatherSearch(btnValue);
 });
 
-forecastWeatherCards('Minneapolis');
+forecastWeatherCards(location);
 
-// switch (icon) {
+ //+ 'Minneapolis, Minnesota'
 
-
-//     case "":
-//         weatherIcon = '<h1 class="icon ml-5"><i class="weather-logo fas fa-cloud-sun"></i> Clouds</h1>`;
-//         break;
-
-
-
-// }
+// var = document.createElement('p');
+//    .setAttribute("id", "")
+//    .setAttribute("class", "")
+//    .textContent= "" + + "";
+//    dash.appendChild();
